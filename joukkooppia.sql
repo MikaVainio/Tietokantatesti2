ca203f65eda6de35c7fea508bb736c6cb59f8ba7
Mika Vainio <koti.mika.vainio@gmail.com>
2020-11-16 15:24:32 +0200

    Create joukkooppia.sql

---
+++ joukkooppia.sql
@@ -0,0 +1,43 @@
+-- Siirretään työntekijä eläkkeelle
+
+-- Varmistetaan, että skripti suoritetaan oikeassa tietokannassa
+USE Joukkooppi
+GO
+
+-- Lisätään Eläkeläinen2020-tauluun uusi tietue
+INSERT dbo.Eläkeläinen2020 (HenkilöID, Etunimi, Sukunimi)
+-- Valitaan siirrettävä tiettue
+(SELECT dbo.Työntekijä2020.HenkilöID, dbo.Työntekijä2020.Etunimi, dbo.Työntekijä2020.Sukunimi
+FROM Työntekijä2020
+WHERE HenkilöID IN (5,6))
+
+
+-- Haetaan työntekijät ja eläkeläiset vuodelta 2020 (ei duplikaatteja)
+USE Joukkooppi
+GO
+
+SELECT HenkilöID, Etunimi, Sukunimi
+FROM Työntekijä2020
+UNION
+SELECT HenkilöID, Etunimi, Sukunimi
+FROM Eläkeläinen2020
+
+-- Haetaan vuonna 2020 eläköityneet so leikkaus työntekijä ja eläkeläiset 2020
+USE Joukkooppi
+GO
+
+SELECT HenkilöID, Etunimi, Sukunimi
+FROM Työntekijä2020
+INTERSECT
+SELECT HenkilöID, Etunimi, Sukunimi
+FROM Eläkeläinen2020
+
+-- Haetaan työntekijät jotka eivät esiinny eläkelistalla
+USE Joukkooppi
+GO
+
+SELECT HenkilöID, Etunimi, Sukunimi
+FROM Työntekijä2020
+EXCEPT
+SELECT HenkilöID, Etunimi, Sukunimi
+FROM Eläkeläinen2020
