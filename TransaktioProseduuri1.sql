cc904242ff7013746e381e4a7c5623df2b7fd5e8
Mika Vainio <koti.mika.vainio@gmail.com>
2020-11-30 14:17:38 +0200

    Add files via upload

---
+++ TransaktioProseduuri1.sql
@@ -0,0 +1,32 @@
+USE [Joukkooppi]
+GO
+
+/****** Object:  StoredProcedure [dbo].[SiirraElakkeelle]    Script Date: 23.11.2020 14.51.04 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- Luodaan proseduuri ja määritellään sen parametri
+CREATE PROCEDURE [dbo].[SiirraElakkeelle]
+	@henkilöid INT = 0
+AS
+BEGIN
+-- Kytketään automaattinen ROLLBACK päälle
+	SET XACT_ABORT ON
+
+-- KAKSIVAIHEINEN ELÄKESIIRTO
+	BEGIN TRANSACTION Eläkesiirto
+	-- Lisästään työntekijä, jonka id on 7 eläkeläisiin
+		INSERT dbo.Eläkeläinen2020 (HenkilöID, Etunimi, Sukunimi)
+		(SELECT HenkilöID, Etunimi, Sukunimi
+		FROM dbo.Työntekijä2020
+		WHERE HenkilöID = @henkilöid)
+	-- Poistetaan henkilö työntekijöistä
+		DELETE FROM dbo.Työntekijä2020
+		WHERE HenkilöID = @henkilöid
+	COMMIT TRANSACTION
+END
+GO
+
